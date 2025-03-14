b1abb6e7fb009ce1b4daa5ee61ee60474027c5bf
maxverwiebe <summe.business@gmail.com>
2025-03-14 13:11:19 +0100

    Fixed CORS in backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     from app.routes.question5 import question5_bp
+    from app.routes.question4 import question4_bp
 
     app.register_blueprint(question5_bp, url_prefix='/api/question5')
+    app.register_blueprint(question4_bp, url_prefix='/api/question4')
 
     @app.route('/')
     def index():
