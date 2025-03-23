3987178e0df3b08a19196e8e8bbd2e318100a4a2
Max <66183307+maxverwiebe@users.noreply.github.com>
2025-03-23 15:57:44 +0100

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask_cors import CORS
+#from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+    #CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
     from app.routes.question5 import question5_bp
     from app.routes.question4 import question4_bp
